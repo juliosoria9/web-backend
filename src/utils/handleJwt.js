8911fd94599a83c4cccbca66febf439b4b376eba
juliosoria9 <162936156+juliosoria9@users.noreply.github.com>
2026-04-09 17:42:00 +0200

    utilidades para gestion de tokens jwt

---
+++ src/utils/handleJwt.js
@@ -0,0 +1,27 @@
+// src/utils/handleJwt.js
+import jwt from 'jsonwebtoken'
+import { config } from '../config/index.js'
+
+export const tokenSign = (user) => {
+  return jwt.sign(
+    { _id: user._id, role: user.role },
+    config.jwt.secret,
+    { expiresIn: config.jwt.expiresIn }
+  )
+}
+
+export const tokenSignRefresh = (user) => {
+  return jwt.sign(
+    { _id: user._id },
+    config.jwt.refreshSecret,
+    { expiresIn: config.jwt.refreshExpiresIn }
+  )
+}
+
+export const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, config.jwt.secret)
+  } catch {
+    return null
+  }
+}
